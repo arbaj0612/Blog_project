6f6f44574d68cf637ade951dce586cb76a288b09
arbaj0612 <junaid.786mulani@gmail.com>
2024-02-22 12:29:56 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo123 from "../assets/blogimg-removebg-preview.png"
+
+
+function Logo() {
+  return (
+    <div>
+    <img src={Logo123} alt="Logo img" style={{ width: "120px" }} />
+    </div>
+  )
+}
+
+export default Logo
